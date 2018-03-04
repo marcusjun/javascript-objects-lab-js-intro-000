6aa9b4d24e1434dbb8442926460ea561f973b71c
Marcus Jun <mjun@calstate.edu>
2018-03-04 17:27:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function updateObjectWithKeyAndValue(object,key,value){
   //return object
   return Object.assign({}, object, { [key]: value })
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
+  object[key]=value
+  return object
+}
