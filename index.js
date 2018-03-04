90962884c7e65c7cde0eb642a5e1dd09e899ce6f
Marcus Jun <mjun@calstate.edu>
2018-03-04 17:36:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,4 +12,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
 function deleteFromObjectByKey(object,key){
   var newRecipes=Object.assign({},object)
   delete newRecipes[key]
+  return newRecipes
 }
