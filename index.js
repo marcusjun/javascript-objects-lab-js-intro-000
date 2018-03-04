73b250d79f98ce40c3aef851db9e270c75996bed
Marcus Jun <mjun@calstate.edu>
2018-03-04 17:24:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var recipes={}
 
 function updateObjectWithKeyAndValue(object,key,value){
-  object[key]=value
-  return object
+  //object[key]=value
+  //return object
+  return Object.assign({},object{[key]:value})
 }
+
